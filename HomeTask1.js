7c4a31c24e92acba2aa2f551d357e244df3772bb
Maksym Kravchuk <kravchukmaxym@gmail.com>
2019-03-27 21:55:01 +0200

    solved tasks in files "HomeTask1" and "HomeTask2"

--- HomeTask1.js
+++ HomeTask1.js
@@ -1,12 +1,19 @@
 //Задача. Створити функцію для виведення на екран (через document.write)
 // малюнка з вказаним URL (URL передається через формальний параметр)
 function showImage(url) {
-    // Записати код тут
+    document.write(`<img src="${url}">`);
 }
 
 
 //Задача. Розробити функцію для формування з повної форми ПІБ скорочену
 //Наприклад з "Іванов Володимир Петрович" => "Іванов В. П."
 function getShortName(fullName) {
-    // Записати код тут
+    let arrPib = fullName.split(" ");
+    let lastName = arrPib[0];
+    let firstName = arrPib[1];
+    let thirdName = arrPib[2];
+    let firstNameSymbol = firstName.split("")[0];
+    let thirdNameSymbol = thirdName.split("")[0];
+
+    return document.write(`${lastName} ${firstNameSymbol}. ${thirdNameSymbol}.`);
 }
--- HomeTask2.js
+++ HomeTask2.js
@@ -1,10 +1,47 @@
 //Задача. Дано масив ідентифікаційних кодів користувачів, які заходили на сайт протягом дня.
 // Розробити функцію, яка б з"ясовувала, чи є у даному масиві вказаний ідентифікаційний номер
-
+function foundId(id, arrId) {
+    for (let i = 0; i < arrId.length; i++) {
+        if (id == arrId[i]) {
+            return console.log(true);
+        }
+    }
+    return console.log(false);
+}
 
 //Задача. Розробити функцію, яка серед заданої послідовності символів (у функцію
 //можна передати довільну кількість символів) виводить тільки голосні
+function vowels() {
+    for (let i = 0; i < arguments.length; i++) {
+
+        if (arguments[i] == "A" || arguments[i] == "a" || arguments[i] == "E" || arguments[i] == "e" 
+        || arguments[i] == "I" || arguments[i] == "i" || arguments[i] == "O" || arguments[i] == "o" 
+        || arguments[i] == "U" || arguments[i] == "u" || arguments[i] == "Y" || arguments[i] == "y") {
+            document.write(arguments[i]);
+        }
+
+    }
+    return;
+}
 
 //Задача. Дано масив вакансій. Вивести на екран скільки разів зустрічається кожна вакансія
 //Приклад: масив вакансіій ["HR","програміст","HR","тестувальник","HR","програміст"]
 //Потрібно вивести: HR - 1,  програміст -2, тестувальник - 1.
+function countVacancy(arrVacancy) {
+    let count = 1;
+    
+    for (let i = 0; i < arrVacancy.length; i++) {
+
+        for (let j = i + 1; j < arrVacancy.length; j++) {
+            if (arrVacancy[i] == arrVacancy[j]) {
+                count++;
+                arrVacancy.splice(j, 1);
+                j--;
+            }
+        }
+
+        document.write(`${arrVacancy[i]} - ${count}, `);
+        count = 1;
+    }
+    return;
+}
