17cbfdd4a6d6de5278bb43b0081741d94f0b3182
Oleksii Klimov <OleksiiKlimov@users.noreply.github.com>
2019-03-27 23:26:48 +0200

    Klimov Homework

--- HomeTask1.js
+++ HomeTask1.js
@@ -2,11 +2,12 @@
 // малюнка з вказаним URL (URL передається через формальний параметр)
 function showImage(url) {
     // Записати код тут
+    return document.write(`<img src=${url} alt="Pic">`)
 }
-
-
 //Задача. Розробити функцію для формування з повної форми ПІБ скорочену
 //Наприклад з "Іванов Володимир Петрович" => "Іванов В. П."
 function getShortName(fullName) {
     // Записати код тут
+    let f = fullName.split(' ')
+    return `${f[0]} ${f[1][0].toUpperCase()}. ${f[2][0].toUpperCase()}.`
 }
--- HomeTask2.js
+++ HomeTask2.js
@@ -1,10 +1,60 @@
 //Задача. Дано масив ідентифікаційних кодів користувачів, які заходили на сайт протягом дня.
 // Розробити функцію, яка б з"ясовувала, чи є у даному масиві вказаний ідентифікаційний номер
-
-
+const IdSearch = (idNum, ...idarr) => {
+    let count = 0
+    for (let i = 0; i < idarr.length; i++) {
+        if (idNum == idarr[i]) count += 1
+    }
+    let ne = ''
+    if (count == 0) ne = "НЕ"
+    return `Користувач з ідентифікаційним номером ${idNum} сьогодні ${ne} заходив`
+}
 //Задача. Розробити функцію, яка серед заданої послідовності символів (у функцію
 //можна передати довільну кількість символів) виводить тільки голосні
-
+const FindVowel = (text) => {
+    let letter = text.split('')
+    let golos = ''
+    for (let i = 0; i < letter.length; i++) {
+        switch (letter[i]) {
+            case 'а': golos += letter[i]
+                break
+            case 'е': golos += letter[i]
+                break
+            case 'є': golos += letter[i]
+                break
+            case 'и': golos += letter[i]
+                break
+            case 'і': golos += letter[i]
+                break
+            case 'ї': golos += letter[i]
+                break
+            case 'о': golos += letter[i]
+                break
+            case 'у': golos += letter[i]
+                break
+            case 'ю': golos += letter[i]
+                break
+            case 'я': golos += letter[i]
+                break
+        }
+    }
+    return golos
+}
 //Задача. Дано масив вакансій. Вивести на екран скільки разів зустрічається кожна вакансія
 //Приклад: масив вакансіій ["HR","програміст","HR","тестувальник","HR","програміст"]
 //Потрібно вивести: HR - 1,  програміст -2, тестувальник - 1.
+
+const vaсancy = (arr) => {
+
+    for (let i = 0; i < arr.length; i++) {
+        let count = 1
+        for (x = i + 1; x < arr.length; x++) {
+            if (arr[i] === arr[x]) {
+                arr.splice(x, 1)
+                count += 1
+            }
+        }
+        document.write(`${arr[i]} - ${count}`)
+    }
+    return
+}
