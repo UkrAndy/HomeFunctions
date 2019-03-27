b9dbbdcbbd1354e18b0609ee9095f41a1fe2c990
Bogdanpayk1993 <body.payk.1993@gmail.com>
2019-03-27 15:24:34 +0200

    Output picture does
    Output picture does

--- HomeTask1.js
+++ HomeTask1.js
@@ -1,9 +1,10 @@
 //Задача. Створити функцію для виведення на екран (через document.write)
 // малюнка з вказаним URL (URL передається через формальний параметр)
 function showImage(url) {
-    // Записати код тут
+    document.write(`<p> <image src="${url}" height="200px"> </p>`)
 }
 
+var a = showImage("https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/share%20img/aventador-coupe-facebook-og.jpg") 
 
 //Задача. Розробити функцію для формування з повної форми ПІБ скорочену
 //Наприклад з "Іванов Володимир Петрович" => "Іванов В. П."
