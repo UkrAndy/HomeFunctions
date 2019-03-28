2ee73c1875917623a61d56a7b76bb8b90ee4f7fd
Bogdanpayk1993 <body.payk.1993@gmail.com>
2019-03-28 11:50:35 +0200

    HomeTask1.js add menu
    HomeTask1.js add menu

--- HomeTask1.js
+++ HomeTask1.js
@@ -4,8 +4,6 @@ function showImage(url) {
     document.write(`<p> <image src="${url}" height="200px"> </p>`)
 }
 
-var a = showImage("https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/share%20img/aventador-coupe-facebook-og.jpg") 
-
 //Задача. Розробити функцію для формування з повної форми ПІБ скорочену
 //Наприклад з "Іванов Володимир Петрович" => "Іванов В. П."
 function getShortName(fullName) {
@@ -14,4 +12,13 @@ function getShortName(fullName) {
     document.write(`<p> ${name_arr} </p>`)
 }
 
-var b = getShortName(prompt("Введіть ім'я"),'')
+var n = prompt("Введіть номер завданя - ",'')
+
+if(n == 1)
+{
+    var a = showImage("https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/share%20img/aventador-coupe-facebook-og.jpg") 
+}
+else
+{
+    var b = getShortName(prompt("Введіть ім'я"),'')
+}
