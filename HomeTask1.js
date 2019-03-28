6ab98448be9d73910bfefc63ae3007c561a18849
TRVPi <sergij.m15@gmail.com>
2019-03-28 22:57:51 +0200

    done

--- HomeTask1.js
+++ HomeTask1.js
@@ -2,11 +2,19 @@
 // малюнка з вказаним URL (URL передається через формальний параметр)
 function showImage(url) {
     // Записати код тут
+    let x = `<img src="${url}" alt="Малюнок">`
+    return x;
 }
-
+document.write(`${showImage('https://www.e1.ru/news/images/resize_900/new1/479/012/images/0.jpg')} <br>`)
 
 //Задача. Розробити функцію для формування з повної форми ПІБ скорочену
 //Наприклад з "Іванов Володимир Петрович" => "Іванов В. П."
 function getShortName(fullName) {
     // Записати код тут
+    let arr = fullName.split(' ');
+    arr[1] = arr[1][0] + '.'
+    arr[2] = arr[2][0] + '.'
+    let str = arr.join(' ')
+    return str
 }
+document.write(`${getShortName('Магдюк Сергій Леонідович')} <br>`)
--- HomeTask2.js
+++ HomeTask2.js
@@ -1,10 +1,46 @@
 //Задача. Дано масив ідентифікаційних кодів користувачів, які заходили на сайт протягом дня.
 // Розробити функцію, яка б з"ясовувала, чи є у даному масиві вказаний ідентифікаційний номер
-
+function ident(arr, number) {
+    let str = arr.indexOf(number)
+    str = str >= 0 ? 'Є такий номер' : 'Нема номеру'
+    return str
+}
+document.write(`${ident([2, 3, 4, 5, 6, 7], 1)} <br>`)
 
 //Задача. Розробити функцію, яка серед заданої послідовності символів (у функцію
 //можна передати довільну кількість символів) виводить тільки голосні
+function letters() {
+    let vowels = ''
+    let str = ''
+    let arrVowels = ['а', 'о', 'у', 'е', 'і', 'и', 'є', 'ї', 'ю', 'я']
+    for (let i = 0; i < arguments.length; i++) {
+        str += arguments[i];
+    }
+    letterArr = str.split('')
+    letterArr.forEach((x, i, arr) => {
+        vowels += arrVowels.indexOf(x) >= 0 ? x : ''
+    });
+    return vowels
+}
+document.write(`${letters('ф', 'ие', 'в', 'а', 'пфффаяп', 'р', 'о', 'д')}<br>`)
 
 //Задача. Дано масив вакансій. Вивести на екран скільки разів зустрічається кожна вакансія
 //Приклад: масив вакансіій ["HR","програміст","HR","тестувальник","HR","програміст"]
-//Потрібно вивести: HR - 1,  програміст -2, тестувальник - 1.
+//Потрібно вивести: HR - 3,  програміст -2, тестувальник - 1.
+
+function vacancy(arr) {
+    let uniq = []
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        uniq.indexOf(arr[i]) < 0 ? uniq.push(arr[i]) : false
+    }
+    for (let i = 0; i < uniq.length; i++) {
+        arr.forEach((x, z, arr) => {
+            x == uniq[i] ? count++ : false
+        });
+        uniq[i] += ' - ' + count
+        count = 0
+    }
+    return uniq
+}
+alert(vacancy(["HR", "програміст", "HR", "тестувальник", "HR", "програміст", "тестувальник", "HR", "програміст", "тестувальник", "HR", "програміст"]))
