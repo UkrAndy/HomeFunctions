ede1060ffc459f431a8d2d238eb9dee6588d89e7
YevhenPopovy4 <har494@gmail.com>
2019-03-28 17:06:18 +0200

    Popovych HomeTask2 work, HomeTask1 need help

--- HomeTask1.js
+++ HomeTask1.js
@@ -2,11 +2,46 @@
 // малюнка з вказаним URL (URL передається через формальний параметр)
 function showImage(url) {
     // Записати код тут
+//var url = <img src="https://www.google.com/search?biw=1242&bih=568&tbm=isch&sa=1&ei=RsKcXKKYOcuKmwXkhbmICg&q=%D1%84%D0%BE%D1%82%D0%BE+nature&oq=%D1%84%D0%BE%D1%82%D0%BE+nature&gs_l=img.3...17371.22157..22284...5.0..0.230.1668.0j10j1......1....1..gws-wiz-img.bngHelE5gbs#imgrc=zXwrfQvPsQ5dwM:" alt="Photo"></img>
+  //  document.write(url)
+    
 }
-
+showImage( )
 
 //Задача. Розробити функцію для формування з повної форми ПІБ скорочену
 //Наприклад з "Іванов Володимир Петрович" => "Іванов В. П."
-function getShortName(fullName) {
+
+
+
+
+
+
+
+
+
+
+/*
+let row = prompt("Enter Numbers","");
+let arr1 = row.split(' ');
+function getShortName(x) {
     // Записати код тут
+    
+    for(var i = 1;i < arr1.length;i++)
+    return x[i]=parseString(arr1[i]).charAt(0); 
 }
+var b=arr1.map(getShortName)
+document.write(b)
+getShortName()
+
+
+
+
+let row = prompt("Enter name","");
+    let arr1 = row.split(' ');
+    let arr =[]
+    var m =[]
+    for (var i=1;i<3;i++){
+         arr[i]=parseString(arr1[i]).charAt(0);
+         return m.push(arr[i])
+        }  
+     document.write(m)*/
